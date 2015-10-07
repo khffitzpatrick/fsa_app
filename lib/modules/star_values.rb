71653b15105dfaf796800f80f68e4b41a4e38430
katharinefitzpatrick <k.h.f.fitzpatrick@gmail.com>
2015-10-07 08:33:37 +0100

    Quick and easy win re-ordering the STAR_MAP so the ratings are in the same order as the acceptance criteria

--- lib/modules/star_values.rb
+++ lib/modules/star_values.rb
@@ -1,10 +1,11 @@
 module Star_Values
 
 	STAR_MAP = {'Exempt' => 'Exempt',
-				'1' => '1-star',
-				'2' => '2-star',
-				'3' => '3-star',
+				'5' => '5-star',
 				'4' => '4-star',
-				'5' => '5-star'}
+				'3' => '3-star',
+				'2' => '2-star',
+				'1' => '1-star'
+			}
 
 end
